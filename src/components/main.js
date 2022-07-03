c53e50726de7598ddf8418b9f7b257c5e4b959d9
Semeiah Bourn <semeiahbourn@gmail.com>
2022-07-03 11:32:13 -0400

    got the question to display with fetch in console

--- src/components/main.js
+++ src/components/main.js
@@ -2,10 +2,22 @@
 import Button from 'react-bootstrap/Button';
 
 function Main (){
+
+const getData = ()=>{
+   
+  fetch('http://jservice.io/api/random')
+    .then(res => res.json())
+    .then(data => {
+      console.log(data[0].question)
+    })
+    .catch(error => console.log(error))
+}
+
     return(
 <div>
     <h1> Jeopardy </h1>
-    <Button variant="primary" size="lg" active>
+    <div id="jeopardyResults"></div>
+    <Button onClick={getData}id="fetchButton" variant="primary" size="lg" active>
         Click Me, Or Don't
       </Button>{' '}
 </div>
