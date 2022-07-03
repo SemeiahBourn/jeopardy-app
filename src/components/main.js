a39339e32761dcbf9dbcbbc24bae21f5e84f5bc3
Semeiah Bourn <semeiahbourn@gmail.com>
2022-07-03 12:48:00 -0400

    broken code

--- src/components/main.js
+++ src/components/main.js
@@ -1,18 +1,25 @@
 
 import Button from 'react-bootstrap/Button';
+import {component} from "react"
 
-function Main (){
-
-const getData = ()=>{
-   
-  fetch('http://jservice.io/api/random')
-    .then(res => res.json())
-    .then(data => {
-      console.log(data[0].question)
-    })
-    .catch(error => console.log(error))
+class Main extends component {
+state = {
+  answer: '',
+  question:'',
+  value:'',
+  title:''  
 }
+    
 
+render(){
+    async function getData () {
+        const url = "http://jservice.io/api/random";
+      const response = await fetch (url);
+      const data = await response.json();
+      this.setState({answer: data[0]})
+       
+       
+      }
     return(
 <div>
     <h1> Jeopardy </h1>
@@ -20,8 +27,12 @@ const getData = ()=>{
     <Button onClick={getData}id="fetchButton" variant="primary" size="lg" active>
         Click Me, Or Don't
       </Button>{' '}
+     <div>{this.state.answer}</div>
+     
 </div>
     )
+}
+
 }
 
 export default Main;
