8cbde39c6fac1818aa8e812a3bd36a2eed786094
Semeiah Bourn <semeiahbourn@gmail.com>
2022-07-03 10:55:14 -0400

    first objective complete button created

---
+++ src/components/main.js
@@ -0,0 +1,15 @@
+
+import Button from 'react-bootstrap/Button';
+
+function Main (){
+    return(
+<div>
+    <h1> Jeopardy </h1>
+    <Button variant="primary" size="lg" active>
+        Click Me, Or Don't
+      </Button>{' '}
+</div>
+    )
+}
+
+export default Main;
