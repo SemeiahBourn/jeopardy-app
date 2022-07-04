9582c0a6f5c1e680164f38a1a85f97e6d5e9704c
Semeiah Bourn <semeiahbourn@gmail.com>
2022-07-03 23:27:21 -0400

    added reset button

--- src/components/main.js
+++ src/components/main.js
@@ -7,11 +7,18 @@ function Main() {
     category: "",
     value: "",
     answer: "",
-    answerVisible:false
+    answerVisible: false,
   });
-  
+  const [score, setScore] = useState(0);
+
+  function watermelon(increment) {
+    setScore(score + increment);
+  }
+  function reset (){
+    window.location.reload();
+  }
+
   async function getData() {
-    
     fetch("http://jservice.io/api/random")
       .then((res) => res.json())
       .then((data) =>
@@ -21,42 +28,60 @@ function Main() {
           value: data[0].value,
           category: data[0].category.title,
           answer: data[0].answer,
-          answerVisible : false
+          answerVisible: false,
         })
-        
       )
 
       .catch((error) => console.log(error));
-
-     
   }
-  function showAnswer (){
-setAllValues({...allValues, answerVisible: true})
-   }
+  function showAnswer() {
+    setAllValues({ ...allValues, answerVisible: true });
+  }
 
   return (
     <div>
       <h1> Jeopardy </h1>
-      <br/>
+      <h3> Score:{score} </h3>
+      <br />
+      {/** The line below is ther because of the error react provided "Uncaught Invariant Violation: Too many re-renders. React limits the number of renders to prevent an infinite loop" */}
+      <Button
+        onClick={() => watermelon(1)}
+        id="increaseButton"
+        variant="primary"
+        size="lg"
+        active
+      > Increase Score</Button>{" "}
+      <Button
+        onClick={() => watermelon(-1)}
+        id="decreaseButton"
+        variant="primary"
+        size="lg"
+        active
+      > Decrease Score </Button>{" "}
+      <Button onClick={reset} id="resetButton" variant="primary" size="lg" active>
+        Reset Game
+      </Button>{" "}
       <div id="jeopardyResults"></div>
-   
-      
       <div>
-       <h1> Question:{allValues.question} </h1> 
-       <br/>
-       <h2> Category:{allValues.category} </h2> 
-       <br/>
+        <br />
+        <h1> Question:{allValues.question} </h1>
+        <br />
+        <h2> Category:{allValues.category} </h2>
+        <br />
         <h2> Points: {allValues.value} </h2>
-        <br/>
-        <h2> Answer: {!allValues.answerVisible ? (
-        <div> </div> )
-        :(
+        <br />
+        <h2>
+          {" "}
+          Answer:{" "}
+          {!allValues.answerVisible ? (
+            <div> </div>
+          ) : (
             <div>
-                <div>{allValues.answer}</div>
+              <div>{allValues.answer}</div>
             </div>
-        )} </h2>
+          )}{" "}
+        </h2>
       </div>
-
       <Button
         onClick={getData}
         id="fetchButton"
@@ -66,10 +91,8 @@ setAllValues({...allValues, answerVisible: true})
       >
         Click Me, Or Don't
       </Button>{" "}
-
       <Button
-      onClick={showAnswer}
-    
+        onClick={showAnswer}
         id="answerButton"
         variant="primary"
         size="lg"
